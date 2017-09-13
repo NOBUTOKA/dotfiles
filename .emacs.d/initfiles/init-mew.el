d5b7987fccf54f1281f7dd5bc17c5677322da6fe
Nobuto <nobutoka@gmail.com>
2017-09-14 07:43:14 +0900

    いろいろ

--- .emacs.d/initfiles/init-mew.el
+++ .emacs.d/initfiles/init-mew.el
@@ -4,3 +4,14 @@
 
 ;; Optional setup (Read Mail menu):
 (setq read-mail-command 'mew)
+
+(autoload 'mew-user-agent-compose "mew" nil t)
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'mew-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'mew-user-agent
+      'mew-user-agent-compose
+      'mew-draft-send-message
+      'mew-draft-kill
+      'mew-send-hook))
