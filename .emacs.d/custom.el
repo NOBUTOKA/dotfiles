5d302c20ccd9a61e9f6be5ede900538a7731cb4e
Nobuto Kaitoh <nobutoka@gmail.com>
2017-10-31 01:19:29 +0900

    org-modeとLaTeXの連携まわり

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(irony-additional-clang-options (quote ("-std=c++11")))
- '(package-selected-packages (quote (codic bind-key all-ext))))
+ '(package-selected-packages (quote (niconama codic bind-key all))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
