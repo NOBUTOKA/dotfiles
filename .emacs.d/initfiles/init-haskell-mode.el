658c615147a2048864f88189a0afd737ff66efbf
Nobuto <nobutoka@gmail.com>
2018-02-19 17:41:47 +0900

    Haskellの環境をinteroに切りかえ

--- .emacs.d/initfiles/init-haskell-mode.el
+++ .emacs.d/initfiles/init-haskell-mode.el
@@ -4,28 +4,15 @@
 (add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
 (add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
 
-;; indent の有効.
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'font-lock-mode)
 (add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook 'intero-mode)
 
 (add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))
 (add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode))
-
-(add-hook 'haskell-mode-hook
-	  #'(lambda()
-	      (interactive)
-	      ;; インデント
-	      (turn-on-haskell-indentation)
-	      (turn-on-haskell-doc-mode)
-	      (font-lock-mode)
-	      (imenu-add-menubar-index)
-	      ;; GHCi のコマンドを設定
-	      (setq haskell-program-name-with-args '("/usr/bin/stack" . "-O"))
-	      (interactive-haskell-mode)
-	      )
-	  )
 
 (with-eval-after-load-feature 'flycheck
   (add-hook 'haskell-mode-hook
@@ -33,8 +20,3 @@
              (setq flycheck-checker 'haskell-hlint)
              (flycheck-mode 1))))
 
-
-(defadvice inferior-haskell-load-file (after change-focus-after-load)
-  "Change focus to GHCi window after C-c C-l command"
-  (other-window 1))
-(ad-activate 'inferior-haskell-load-file)
--- .emacs.d/initfiles/install_haskell.el
+++ .emacs.d/initfiles/install_haskell.el
@@ -1,3 +1,2 @@
 (el-get-bundle haskell-mode)
-(el-get-bundle ghc-mod)
-(el-get-bundle company-ghc)
+(el-get-bundle intero)
