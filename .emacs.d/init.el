8885c0044264cf2c67d94f78b927a21e9834432e
Nobuto Kaitoh <nobutoka@gmail.com>
2026-02-14 21:19:23 +0900

    ファイル構造を整理、その3(一旦完了)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,9 +15,32 @@
   (when (file-exists-p init_host)
     (load-file init_host)))
 
-;; initialize general config
-(when (file-exists-p (concat user-emacs-directory "initfiles/init_general.el"))
-  (load (concat user-emacs-directory "initfiles/init_general.el")))
+
+;; color theme config
+(load-theme 'modus-vivendi t)
+
+;;language config
+(set-language-environment "UTF-8")
+(prefer-coding-system 'utf-8-unix)
+
+;; Activate fido-vertical-mode (Fallback for vertico)
+(when (version<= "28" emacs-version)
+  (fido-vertical-mode 1)
+  (define-key icomplete-fido-mode-map (kbd "<DEL>") #'delete-backward-char)
+  (define-key icomplete-fido-mode-map (kbd "C-l") #'icomplete-fido-backward-updir))
+
+;; Straight.el bootstrap
+(load (locate-user-emacs-file "bootstrap-straight.el"))
+
+;; Install leaf (using straight.el)
+(straight-use-package 'leaf)
+(straight-use-package 'leaf-keywords)
+(leaf-keywords-init)
+
+(load (locate-user-emacs-file "initfiles/init-core.el"))
+(load (locate-user-emacs-file "initfiles/init-ui.el"))
+(load (locate-user-emacs-file "initfiles/init-completions.el"))
+(load (locate-user-emacs-file "initfiles/init-languages.el"))
 
 (let* ((init_host (concat user-emacs-directory "init_" (system-name) "_aft.el")))
   (when (file-exists-p init_host)
